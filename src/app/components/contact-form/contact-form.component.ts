f6f5202f4fcb3ef9a8d7f72180a2c6159d8ff2ae
Krishan Dharmasiri <krishantha.dharmasiri@outlook.com>
2023-10-17 18:55:06 +0530

    more bootstrap input controls

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -9,6 +9,12 @@ import { Contact } from 'src/app/interfaces/contact';
 export class ContactFormComponent {
   
   myContact = <Contact>{};
+  languages = [
+    {id:1, name : 'C-sharp'},
+    {id:2, name : 'JavaScripts'},
+    {id:3, name : 'Azure'},
+    {id:4, name : 'Python'}
+  ]
 
   onSubmit(){
     console.log(this.myContact);
