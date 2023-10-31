a84226904b3a45377002220daae21791ad73897e
Krishan Dharmasiri <krishantha.dharmasiri@outlook.com>
2023-10-31 19:15:11 +0530

    refactored the http code to use switchMap

--- src/app/components/github/github.component.ts
+++ src/app/components/github/github.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GithubDataService } from 'src/app/services/github-data.service';
-import { combineLatest } from 'rxjs';
+import { combineLatest, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-github',
@@ -18,7 +18,32 @@ export class GithubComponent implements OnInit {
   constructor(private _activatedRoute : ActivatedRoute, private _githubDataService : GithubDataService){}
 
   ngOnInit(): void {
+    combineLatest([
+      this._activatedRoute.paramMap,
+      this._activatedRoute.queryParamMap
+    ]).pipe(
+      switchMap(combined => {
+        this.user = combined[0].get('user');
+        this.pageNumber = combined[1].get('page');
+        this.order = combined[1].get('order');
 
+        console.log(this.user);
+        console.log(this.pageNumber);
+        console.log(this.order);
+
+        return this._githubDataService.getAll();
+      })      
+    )
+    .subscribe({
+      next : (followers => {
+        this.followers = followers;
+        console.log(followers);
+      })
+    })
+    
+  }
+
+  private getDataFromService_v1(){
     combineLatest([
       this._activatedRoute.paramMap,
       this._activatedRoute.queryParamMap
